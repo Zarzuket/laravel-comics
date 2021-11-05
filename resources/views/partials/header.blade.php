c6d3950f62ac3621d7edf31697c15680eb096d6d
Zarzuket <davide.bajo@gmail.com>
2021-11-05 15:47:40 +0100

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <nav>
+        <img src="" alt="">
+        <ul>
+            <li><a href="">CHARACTERS</a></li>
+            <li><a href="">COMICS</a></li>
+            <li><a href="">MOVIES</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">GAMES</a></li>
+            <li><a href="">COLLECTIBLES</a></li>
+            <li><a href="">VIDEO</a></li>
+            <li><a href="">FANS</a></li>
+            <li><a href="">NEWS</a></li>
+            <li><a href="">SHOP</a></li>
+        </ul>
+    </nav>
+</header>

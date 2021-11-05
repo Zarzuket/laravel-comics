da287478eebe94512ec8bbecd7336c53062f7e1a
Zarzuket <davide.bajo@gmail.com>
2021-11-05 16:19:56 +0100

    add header + scss + images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,9 @@
 @extends('layout.base')
+
+@section('Content')
+   <section class="section_main">
+       <div class="container">
+        
+       </div>
+   </section>
+@endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,17 +1,20 @@
 <header>
-    <nav>
-        <img src="" alt="">
-        <ul>
-            <li><a href="">CHARACTERS</a></li>
-            <li><a href="">COMICS</a></li>
-            <li><a href="">MOVIES</a></li>
-            <li><a href="">TV</a></li>
-            <li><a href="">GAMES</a></li>
-            <li><a href="">COLLECTIBLES</a></li>
-            <li><a href="">VIDEO</a></li>
-            <li><a href="">FANS</a></li>
-            <li><a href="">NEWS</a></li>
-            <li><a href="">SHOP</a></li>
-        </ul>
-    </nav>
+        <nav>
+            <img src="{{asset('images/dc-logo.png')}}" alt="">
+            <ul>
+                <li><a href="">CHARACTERS</a></li>
+                <li><a href="">COMICS</a></li>
+                <li><a href="">MOVIES</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">GAMES</a></li>
+                <li class="active"><a href="">COLLECTIBLES</a></li>
+                <li><a href="">VIDEO</a></li>
+                <li><a href="">FANS</a></li>
+                <li><a href="">NEWS</a></li>
+                <li><a href="">SHOP</a></li>
+            </ul>
+        </nav>
+    <section class="jumbo">
+
+    </section>
 </header>

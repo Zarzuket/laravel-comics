ce7b91e8488b6bb07ff9a0d352c33b48ca17b05f
Zarzuket <davide.bajo@gmail.com>
2021-11-05 17:19:19 +0100

    add comics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,15 @@
 @section('Content')
    <section class="section_main">
        <div class="container">
-        
+           <div class="container_comics">
+               @foreach ($comics as $comic)
+               <div class="card">
+                   <img class="card_img" src="{{$comic["thumb"]}}" alt="com.series">
+                   <div class="card_title">
+                     {{$comic["series"]}}
+                   </div>
+           </div>
+            @endforeach
        </div>
    </section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-
-    return view('index');
+    $data = config("comics");
+    // dd($data);
+    $tmp = [
+        "comics" => $data
+    ];
+    return view('index',$tmp);
 });
